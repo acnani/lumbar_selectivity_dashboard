4750e96da25bd27899d09f89b53dd0e9362a9d73
Ameya <acnani@gmail.com>
2020-06-18 13:16:19 -0400

    initial commit for public repository

---
+++ publicDashboard.py
@@ -0,0 +1,314 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import utils.dash_reusable_components as drc
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import numpy as np
+import base64
+import pandas as pd
+import urllib
+import publicHelper as hf
+import os
+
+client = hf.client
+db = hf.db
+collection = hf.collection
+
+app = dash.Dash(__name__)
+app.config['suppress_callback_exceptions']=True
+app.title = 'Lumbar Selectivity Viewer'
+server = app.server
+
+dataDF = pd.DataFrame()
+
+anonymizedList = {'Cat E':'Electro',
+                  'Cat F':'Freeze',
+                  'Cat G':'Galactus',
+                  'Cat H':'Hobgoblin',
+                  'Cat I':'HA02',
+                  'Cat J':'HA04',}
+
+app.layout = html.Div(children=[
+    # .container class is fixed, .container.scalable is scalable
+    html.Div(className="banner", children=[
+        # Change App Name here
+        html.Div(className='container scalable', children=[
+            # Change App Name here
+            html.H2(html.A('Lumbar DRG selectivity viewer',
+                # href='https://github.com/plotly/dash-svm',
+                style={
+                    'text-decoration': 'none',
+                    'color': 'inherit'
+                }
+            )),
+            html.Div(
+                html.A(
+                    html.Img(
+                        src='data:image/png;base64,{}'.format(base64.b64encode(open(os.path.join("RNEL_logo.png"),'rb').read())),height="100%",),
+                    href='http://www.rnel.pitt.edu/', target='_blank'
+                ),
+            style={"float": "right", "height": "70%",})
+        ]),
+    ]),
+
+        html.Div(
+                className='three columns',
+                children=[
+                    drc.Card([
+
+                        drc.NamedDropdown(
+                            name='Select Electrode: ',
+                            id='dropdown-select-eType',
+                            options=[
+                                {'label': 'epineural', 'value': 'epineural'},
+                                {'label': 'penetrating', 'value': 'penetrating'},
+                            ],
+                            value='epineural',
+                            clearable=False,
+                            searchable=False,
+                        ),
+
+
+                        drc.NamedDropdown(
+                            name='Select Subject: ',
+                            id='dropdown-select-subject',
+                            options=[
+                            ],
+                            clearable=False,
+                            searchable=False,
+                            # value='moons'
+                        ),
+
+                        drc.NamedDropdown(
+                            name='Select Session: ',
+                            id='dropdown-select-session',
+                            options=[{}],
+                            clearable=False,
+                            searchable=False,
+                        ),
+
+                        drc.NamedDropdown(
+                            name='Select Stim channel: ',
+                            id='dropdown-select-stimChan',
+                            options=[{}],
+                            clearable=False,
+                            searchable=False,
+                        ),
+
+                        drc.NamedDropdown(
+                            name='Select Stim amp: ',
+                            id='dropdown-select-stimAmp',
+                            options=[{}],
+                            clearable=False,
+                            searchable=False,
+                        ),
+
+                        html.A("Download CSV",
+                               id='download_link',
+                               download="rawdata.csv",
+                               href="",
+                               target="_blank"
+                               ),
+                    ]),
+                    drc.Card(['Select the desired trial of DRG stimulation from the dropdown menus above. The graph on the top right will '
+                              'display the stimulus triggered average (STA) electroneurogram (ENG) recorded at distal nerve branches of the sciatic and femoral nerve. '
+                              'The graph below will display an innervation tree where each node represents a nerve. The color of each node '
+                              'represents the minimum stimulation amplitude (or activation threshold) that produced a compound action potential (CAP) for the selected stimulation channel. '
+                              'Hover over each node for nerve identity and threshold. You can also download the data for each trial as a CSV file.']),
+                ]
+            ),
+    html.Div(
+        className='nine columns',
+        children=[
+            drc.Card([
+                html.Div(
+                    id='div-graphs',
+                    children=dcc.Graph(
+                        id='STA_ENG',
+                    )
+                ),
+            ]),
+
+            drc.Card([
+                html.Div(
+                    id='div-graphs2',
+                    children=dcc.Graph(
+                        id='innervationTree',
+                        style={"height": "33vh"},
+                    )
+                ),
+            ]),
+        ]),
+])
+
+
+@app.callback (Output('dropdown-select-subject', "options"),
+               [Input('dropdown-select-eType', 'value')],)
+def getSubs(eTypeVal):
+    subList = []
+    if eTypeVal == 'epineural':
+        subList =  ['Cat G','Cat H','Cat I','Cat J']
+    elif eTypeVal == 'penetrating':
+        subList =  ['Cat E','Cat F','Cat G','Cat H']
+    
+    return [{'label': iSub, 'value': anonymizedList[iSub]} for iSub in subList]
+
+@app.callback (Output('dropdown-select-subject', "value"),
+               [Input('dropdown-select-eType', 'value')],)
+def getSubVal(subjVal):
+    return ''
+
+
+@app.callback (Output('dropdown-select-session', "options"),
+               [Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getSesh(subjVal, eTypeVal):
+    seshDrop = []
+    if subjVal:
+        seshList = []
+        if eTypeVal == 'epineural':
+            seshList =  hf.epineuralSessions[subjVal]
+        elif eTypeVal == 'penetrating':
+            seshList =  hf.penetratingSessions[subjVal]
+        seshDrop = []
+        for iSesh in sorted(seshList):
+            iDRG = db[collection].find({'subject':subjVal,'session':iSesh}).distinct('DRG')
+            seshDrop.append({'label': 'session %d (%s)'%(iSesh, iDRG[0]), 'value': iSesh})
+
+    return seshDrop
+
+@app.callback (Output('dropdown-select-session', "value"),
+               [Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getSeshVal(subjVal, eTypeVal):
+    return ''
+
+
+@app.callback (Output('dropdown-select-stimChan', "options"),
+               [Input('dropdown-select-session', "value"),
+                Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getStimChan(seshVal, subjVal, eTypeVal):
+    chanList = sorted(list(db[collection].find({'subject':subjVal,
+                           'session': seshVal}).distinct('stimChan')))
+    return [{'label': iChan, 'value': iChan} for iChan in chanList]
+
+@app.callback (Output('dropdown-select-stimChan', "value"),
+               [Input('dropdown-select-session', "value"),
+                Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getStimChanVal(seshVal, subjVal, eTypeVal):
+    return ''
+
+
+@app.callback (Output('dropdown-select-stimAmp', "options"),
+               [Input('dropdown-select-stimChan', "value"),
+                Input('dropdown-select-session', "value"),
+                Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getStimAmp(stimChan, seshVal, subjVal, eTypeVal):
+    ampList = sorted(list(db[collection].find({'subject':subjVal,
+                                                     'stimChan': stimChan,
+                                          'session': seshVal}).distinct('amplitude')))
+
+    return [{'label': round(iAmp,2), 'value': round(iAmp,2)} for iAmp in ampList]
+
+@app.callback (Output('dropdown-select-stimAmp', "value"),
+               [Input('dropdown-select-stimChan', "value"),
+                Input('dropdown-select-session', "value"),
+                Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getAmpVal(stimChan, seshVal, subjVal, eTypeVal):
+    return ''
+
+
+@app.callback (Output('STA_ENG', "figure"),
+               [Input('dropdown-select-stimAmp', "value"),
+                Input('dropdown-select-stimChan', "value"),
+                Input('dropdown-select-session', "value"),
+               Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def getSTAENGsnips(amp, stimChan, sesh, subj, eTypeVal):
+
+    if amp:
+        engObj = list(db[collection].find({'subject': subj,
+                                                 'session': sesh,
+                                                 'amplitude': {"$lte": amp + 0.1, "$gte": amp - 0.1},
+                                                 'stimChan': stimChan}))
+
+        figData = []
+        global dataDF
+        dataDF = pd.DataFrame()
+        for iObj in engObj:
+            yENG = iObj['wf']
+            numSamples = len(yENG)
+            xtime = list(np.linspace(0, float(numSamples) / 30, numSamples))
+            figData.append(go.Scatter(
+                x=xtime,
+                y=yENG,
+                mode='lines',
+                line=dict(shape='spline'),
+                name = iObj['location'],
+                hoverinfo = 'none'
+            ))
+
+            dataDF[iObj['location']] = yENG
+
+        dataDF['time (ms)'] = xtime
+        layout = dict(margin=dict(l=60, r=10, t=0, b=70),  legend=dict(yanchor="bottom",y=0),
+                      xaxis=dict(title='Time (ms)'),
+                      yaxis=dict(fixedrange=True, title='amp (uV)'))
+
+        return dict(data=figData, layout=layout)
+    else:
+        return dict(data=[], layout=[])
+
+@app.callback (Output('innervationTree', "figure"),
+               [Input('dropdown-select-stimChan', "value"),
+                Input('dropdown-select-session', "value"),
+                Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def createInnervationTreeDiagram(chan, session, subject, eTypeVal):
+    if chan:
+        allThreshDict = hf.thresholdPerCuff(subject, session, chan, ['BiFem'], True)
+        if chan in allThreshDict.keys():
+            threshDict = allThreshDict[chan]
+            resultCuffs = threshDict.keys()
+            allAmps = [threshDict[cuffName] for cuffName in resultCuffs]
+            tmp = hf.generateInnervationTree(resultCuffs, allAmps, 25)
+            tmp['layout']['title'] = tmp['layout']['title'] + ' (hover for activation threshold) '
+            tmp['layout']['margin'] = dict(t=30, b=20)
+            return tmp
+        else:
+            return dict(data=[], layout=[])
+    else:
+        return dict(data=[], layout=[])
+
+
+@app.callback (Output('download_link', "download"),
+               [Input('dropdown-select-stimAmp', "value"),
+                Input('dropdown-select-stimChan', "value"),
+                Input('dropdown-select-session', "value"),
+               Input('dropdown-select-subject', 'value'),
+                Input('dropdown-select-eType', 'value')],)
+def setFileName(amp, stimChan, sesh, subj, eTypeVal):
+    if amp:
+        return '%s_ssn%03d_chan%02d_amp%0.2f_%s.csv' %(subj, sesh, stimChan, amp,eTypeVal)
+
+
+@app.callback (Output('download_link', 'href'),
+               [Input('download_link', "n_clicks"),
+                Input('STA_ENG', "figure"),
+                Input('dropdown-select-stimAmp', "value")],)
+def downloadDF(n, fig, amp):
+    if amp:
+        global dataDF
+        csv_string = dataDF.to_csv(index=False, encoding='utf-8')
+        csv_string = "data:text/csv;charset=utf-8," + urllib.quote(csv_string)
+        return csv_string
+
+# Running the server
+if __name__ == '__main__':
+    app.run_server(debug=False)
+    # app.run_server(debug=True)
---
+++ publicHelper.py
@@ -0,0 +1,304 @@
+import pymongo
+import matplotlib
+from matplotlib import cm
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+import collections
+import plotly.graph_objs as go
+from igraph import *
+import os
+
+print os.environ['mongoString']
+client = pymongo.MongoClient(os.environ['mongoString'])
+
+db = client.acute
+collection = 'selectivity'
+
+# cuff dbName:label
+allCuffs_mdf = collections.OrderedDict([('Sciatic_Proximal','Sci'),
+                                        ('Tibial','Tib'),
+                                        ('Lat_Gas','LG'),
+                                        ('Med_Gas','MG'),
+                                        ('Dist_Tib','dTib'),
+                                        ('Cmn_Per','CP'),
+                                        ('Dist_Cmn_Per','dCP'),
+                                        ('Dist_Cmn_Per_2','dCP2'),
+                                        ('L_D_Cmn_Per','SP'),
+                                        ('M_D_Cmn_Per','DP'),
+                                        ('BiFem','BF'),
+                                        ('Sural','Sur'),
+                                        ('Lat_Cut','LC'),
+                                        ('Med_Cut','MC'),
+                                        ('Sens_Branch','Sensory'),
+                                        ('Femoral_Proximal', 'Fem'),
+                                        ('Saph', 'Sph'),
+                                        ('VLat', 'VL'),
+                                        ('VMed', 'VM'),
+                                        ('Sart', 'Srt'),
+                                        ])
+
+combineCuffs = {'Sural':'Sens_Branch',
+                'Lat_Cut':'Sens_Branch',
+                'Med_Cut':'Sens_Branch',
+                'Sens_Branch':'Sens_Branch',
+                'Dist_Cmn_Per':'Dist_Cmn_Per',
+                'Dist_Cmn_Per_2':'Dist_Cmn_Per',
+                'L_D_Cmn_Per':'Dist_Cmn_Per',
+                'M_D_Cmn_Per':'Dist_Cmn_Per'}
+
+PWbySession = {'Electro':{20:204.8, 22:204.8, 26:204.8, 27:204.8, 28:204.8, 32:204.8},
+               'Freeze':{55:204.8, 56:204.8, 59:204.8, 60:204.8, 61:204.8, 63:204.8, 68:204.8, 999:204.8},
+                'Galactus':{15:204.8, 30:81.92, 40:81.92, 41:81.92, 48:81.92, 57:81.92,
+                           91:81.92, 94:81.92, 97:81.92, 98:81.92},
+                'Hobgoblin':{6:81.92, 7:81.92, 10:81.92, 12:81.92, 14:81.92, 16:81.92, 20:81.92, 23:81.92,
+                             47:81.92, 49:81.92, 52:81.92},
+                'HA02':{2:204.8, 3:204.8, 4:204.8},
+                'HA04':{2:80, 3:80, 4:80}}
+
+epineuralSessions = {'Galactus':[15, 30, 40, 41, 48],
+                     'Hobgoblin':[6, 7, 10, 12, 14, 16],
+                     'HA02':[2, 3, 4],
+                     'HA04':[2, 3, 4]}
+
+penetratingSessions = {'Electro':[20, 22, 26, 27, 28, 32],
+                       'Freeze':[55, 56, 59, 60, 61, 63, 999],
+                       'Galactus':[91, 94, 97, 98],
+                       'Hobgoblin':[47, 49, 52]}
+
+## create heatmap colormap
+def createMagmaCMAP():
+    magma_cmap = matplotlib.cm.get_cmap('magma')
+    magma_rgb = []
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=255)
+
+    for i in range(0, 255):
+        k = matplotlib.colors.colorConverter.to_rgb(magma_cmap(norm(i)))
+        magma_rgb.append(k)
+
+    h = 1.0 / (255 - 1)
+    pl_colorscale = []
+
+    for k in range(255):
+        C = map(np.uint8, np.array(magma_cmap(k * h)[:3]) * 255)
+        pl_colorscale.append([k * h, 'rgb' + str((C[0], C[1], C[2]))])
+
+    return pl_colorscale
+magma = createMagmaCMAP()
+
+def convertCurrentToCharge(amplitude_uA, sub, sesh):
+    return np.ceil((amplitude_uA*1e-6*PWbySession[sub][sesh]* 1e-6*1e9)*1000)/1000   # ensure 2 decimal places
+
+# animal:DRG(session):channel(block):amp:cuff:cv
+def thresholdPerCuff(sub, session, chan,ignoreCuffList, combine, stimUnits='amplitude'):
+    ignoreCuffList.extend(['Sciatic_Distal','Femoral_Distal'])
+    result1 = db[collection].aggregate([
+            {'$match': {
+                "subject": sub,
+                "session": session,
+                "location":{"$nin":ignoreCuffList},
+                "stimChan":chan,
+            }},
+            {"$group": {
+                "_id": {"cuff": "$location",
+                        "stimChan": "$stimChan",
+                        'is_sig':'$is_sig',
+                        'is_sig_manual':'$is_sig_manual'},
+                "threshAmp": {"$min": "$amplitude"}
+            }},
+            {"$project": {
+                "_id": 0,
+                'sig':"$_id.is_sig",
+                'sig_manual':"$_id.is_sig_manual",
+                "stimChan": "$_id.stimChan",
+                "cuff": "$_id.cuff",
+                "threshAmp": "$threshAmp",
+            }}])
+
+    res1 = list(result1)
+    thresholdDict = {}
+    if len(res1) != 0:
+        for entry in res1:
+            if ('sig_manual' in entry.keys() and entry['sig_manual'] == 1) or ('sig_manual' not in entry.keys() and entry['sig'] == 1):
+                thresholdDict.setdefault(entry['stimChan'], {})
+                # if not(entry['cuff'] in ['Sciatic_Distal', 'Femoral_Distal']):
+                if stimUnits =='charge':
+                    stimVal = convertCurrentToCharge(entry['threshAmp'],sub, session)
+                else:
+                    stimVal = entry['threshAmp']
+
+                if entry['cuff'] in combineCuffs.keys():
+                    if combine:
+                        combinedKey = combineCuffs[entry['cuff']]
+                        thresholdDict[entry['stimChan']].setdefault(combinedKey, 999)
+                        tmp = [thresholdDict[entry['stimChan']][combinedKey]]
+                        tmp.append(stimVal)
+                        thresholdDict[entry['stimChan']][combinedKey] = min(tmp)
+                    else:
+                        # in case sig_manual is 1 sig is 1 is greater than sig manual =1 and sig =0, the threshold will be overwritten to a higher value
+                        thresholdDict[entry['stimChan']].setdefault([entry['cuff']],999)
+                        tmp = thresholdDict[entry['stimChan']][entry['cuff']]
+                        thresholdDict[entry['stimChan']][entry['cuff']] = min(tmp, stimVal)
+                        thresholdDict[entry['stimChan']][entry['cuff']] = stimVal
+                else:
+                    thresholdDict[entry['stimChan']].setdefault(entry['cuff'], 999)
+                    tmp = thresholdDict[entry['stimChan']][entry['cuff']]
+                    thresholdDict[entry['stimChan']][entry['cuff']] = min(tmp, stimVal)
+                    # thresholdDict[entry['stimChan']][entry['cuff']] = stimVal
+
+    return thresholdDict
+
+## Innervation Tree related
+def getInnervationParents(): # getCanonicalInnervation
+    innervationDict  = {}
+    innervationDict['Sciatic_Proximal']= ''
+    innervationDict['Cmn_Per']= 'Sciatic_Proximal'
+    innervationDict['Tibial'] = 'Sciatic_Proximal'
+    innervationDict['BiFem'] = 'Sciatic_Proximal'
+    innervationDict['Sural']= 'Sciatic_Proximal'
+    innervationDict['Lat_Cut'] = 'Sciatic_Proximal'
+    innervationDict['Med_Cut']= 'Sciatic_Proximal'
+    innervationDict['Sens_Branch']= 'Sciatic_Proximal'
+    innervationDict['Dist_Cmn_Per'] = 'Cmn_Per'
+    innervationDict['Dist_Cmn_Per_2']= 'Cmn_Per'
+    innervationDict['L_D_Cmn_Per']= 'Dist_Cmn_Per'
+    innervationDict['M_D_Cmn_Per']= 'Dist_Cmn_Per'
+    innervationDict['Med_Gas']= 'Tibial'
+    innervationDict['Lat_Gas']= 'Tibial'
+    innervationDict['Dist_Tib']= 'Tibial'
+    innervationDict['Femoral_Proximal'] = ''
+    innervationDict['Saph']= 'Femoral_Proximal'
+    innervationDict['VMed']= 'Femoral_Proximal'
+    innervationDict['VLat']= 'Femoral_Proximal'
+    innervationDict['Sart'] = 'Femoral_Proximal'
+    # innervationDict['Lat_Fem']= ''
+    # innervationDict['Med_Fem']= ''
+    # innervationDict['Mid_Fem']= ''
+
+    return innervationDict
+
+def getInnervationChildren():
+    innervationDict  = {}
+    innervationDict['Sciatic_Proximal']= ['Cmn_Per', 'Tibial', 'BiFem','Sural','Lat_Cut','Med_Cut','Sens_Branch']
+    innervationDict['Cmn_Per'] = ['Dist_Cmn_Per', 'Dist_Cmn_Per_2']
+    innervationDict['Dist_Cmn_Per'] = ['L_D_Cmn_Per', 'M_D_Cmn_Per']
+    innervationDict['Tibial']= ['Med_Gas', 'Lat_Gas', 'Dist_Tib']
+    innervationDict['Femoral_Proximal'] = ['Saph', 'VMed', 'VLat', 'Sart']
+    innervationDict[''] = ['Femoral_Proximal', 'Sciatic_Proximal']
+
+
+    return innervationDict
+
+def getInnervationTreeCoords():
+    coords = {}
+    coords['Sciatic_Proximal'] = [3.6, 4.0]
+    coords['Cmn_Per'] = [7.9, 3.0]
+    coords['Tibial'] = [5.4, 3.0]
+    coords['BiFem'] = [4.4, 3.0]
+    coords['Sural'] = [3.4, 3.0]
+    coords['Sens_Branch'] = [2.4, 3.0]
+    coords['Med_Cut'] = [1.4, 3.0]
+    coords['Lat_Cut'] = [0.4, 3.0]
+    coords['Dist_Cmn_Per'] = [8.4, 2.0]
+    coords['Dist_Cmn_Per_2'] = [7.4, 2.0]
+    coords['L_D_Cmn_Per'] = [8.9, 1.0]
+    coords['M_D_Cmn_Per'] = [7.9, 1.0]
+    coords['Med_Gas'] = [6.4, 2.0]
+    coords['Lat_Gas'] = [5.5, 2.0]
+    coords['Dist_Tib'] = [4.4, 2.0]
+    coords['Femoral_Proximal'] = [-2.3, 4.0]
+    coords['Saph'] = [-0.8, 3.0]
+    coords['VMed'] = [-1.8, 3.0]
+    coords['VLat'] = [-2.8, 3.0]
+    coords['Sart'] = [-3.8, 3.0]
+
+    return coords
+
+def generateInnervationTree(resultCuffs, nodeColor, nodeSize=40, stimUnits='amplitude', eType='epineural'):
+
+    cuffParentsDict = getInnervationParents()
+    graphEdges = []
+    for cuffName in resultCuffs:
+        if cuffName in cuffParentsDict.keys():
+            parent = cuffParentsDict[cuffName]
+            if parent != '':
+                if parent in resultCuffs:
+                    graphEdges.append([resultCuffs.index(parent), resultCuffs.index(cuffName)])
+
+    cuffCoord = getInnervationTreeCoords()
+    lay = [cuffCoord[iCuff] for iCuff in resultCuffs]
+
+    G = Graph()
+    G.add_vertices(len(resultCuffs))
+    G.add_edges(graphEdges)
+    E = [e.tuple for e in G.es]  # list of edges
+    L = len(lay)
+    Xn = [lay[k][0] for k in range(L)]
+    Yn = [lay[k][1] for k in range(L)]
+    Xe = []
+    Ye = []
+    for edge in E:
+        Xe += [lay[edge[0]][0], lay[edge[1]][0], None]
+        Ye += [lay[edge[0]][1], lay[edge[1]][1], None]
+
+    if isinstance(nodeColor, list):
+        hoverText = ['%s, c%0.1f' % (allCuffs_mdf[x], y) for x,y in zip(resultCuffs,nodeColor)]
+    elif isinstance(nodeSize, list):
+        hoverText = ['%s, c%0.1f' % (allCuffs_mdf[x], y) for x,y in zip(resultCuffs,nodeSize)]
+    else:
+        hoverText = []
+
+    if stimUnits == 'charge':
+        if eType == 'epineural':
+            colorMap = [9,26]
+        else:
+            colorMap = [1,6] #[6, 1]
+    else:                               # current
+        if eType == 'epineural':
+            colorMap = [300, 0]
+        else:
+            colorMap = [40, 0]
+
+    lines = go.Scatter(x=Xe,
+                       y=Ye,
+                       mode='lines',
+                       line=dict(color='rgb(210,210,210)', width=1),
+                       hoverinfo='none'
+                       )
+    dots = go.Scatter(x=Xn,
+                      y=Yn,
+                      mode='markers',
+                      marker=dict(size=nodeSize,
+                                  color=nodeColor,  # '#DB4551',
+                                  cmin=colorMap[0], cmax=colorMap[1],
+                                  line=dict(color='rgb(50,50,50)', width=1),
+                                  colorscale=magma,
+                                  # colorscale=[[0, 'rgb(49,54,149)'],  # 0
+                                  #               [0.0005, 'rgb(69,117,180)'],  # 10
+                                  #               [0.005, 'rgb(116,173,209)'],  # 100
+                                  #               [0.05, 'rgb(171,217,233)'],  # 1000
+                                  #               [0.5, 'rgb(224,243,248)'],
+                                  #               [0.75, 'rgb(215,48,39)'],# 10000
+                                  #               [1.0, 'rgb(165,0,38)'],  # 100000
+                                  #               ],
+                                  colorbar=dict(thickness=20)
+                                  ),
+                      text=hoverText,
+                      hoverinfo='text',
+                      opacity=1
+                      )
+
+    layout = dict(title='Innervation tree',
+                  font=dict(size=12),
+                  showlegend=False,
+                  xaxis=dict(range=[-5, 10], showline=False, zeroline=True, showgrid=False, showticklabels=False, ),
+                  yaxis=dict(range=[1.5, 4.5], showline=False, zeroline=True, showgrid=True, showticklabels=False, ),
+                  margin=dict(l=40, r=40, b=85, t=100),
+                  hovermode='closest',
+                  plot_bgcolor='rgb(248,248,248)'
+                  )
+
+    data = [lines, dots]
+    fig = dict(data=data, layout=layout)
+
+    return fig
+
